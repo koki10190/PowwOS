d79b1e7fb42a79d9dab509a216809ff8b334f358
koki1019 <lukaodzelashvili@gmail.com>
2023-10-31 16:24:01 +0000

    /

--- src/bootloader/src/main.c
+++ src/bootloader/src/main.c
@@ -178,6 +178,10 @@ UINTN __strcmp(CHAR8 *a, CHAR8 *b, UINTN length) {
     return 1;
 }
 
+uint32_t rgb_to_hex(int r, int g, int b) {
+    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
+
 EFI_FILE *LoadFile(EFI_FILE *directory, CHAR16 *path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     EFI_FILE *LoadedFile;
 
